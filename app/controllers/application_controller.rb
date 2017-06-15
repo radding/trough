d5eed541f610e4e21ac20a85408670b0d468c64d
Daniel Choi <danjchoi@umich.edu>
2017-06-15 18:38:38 -0400

    added devise_token_auth and dependencies to gemfille

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
     def index
         render json: {:version => "0.0.1", :status => "ok", :code => "200"}
     end
