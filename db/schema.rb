f9d97a8fedd08372c7795bd16280ae048cd82917
Daniel Choi <danjchoi@umich.edu>
2017-07-11 09:54:52 -0400

    google_place added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629214956) do
+ActiveRecord::Schema.define(version: 20170711135049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 20170629214956) do
   create_table "places", force: :cascade do |t|
     t.string   "name"
     t.string   "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "google_place"
   end
 
   create_table "teams", force: :cascade do |t|
