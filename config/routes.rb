839691dd6e57e94815674e5e940ab9f94a6c01d2
Daniel Choi <danjchoi@umich.edu>
2017-07-11 18:49:36 -0400

    added a join route, adjusted params

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   resources :users  
   resources :tests
   get '/', to: 'application#index'
+  post "/teams/:team_id/outings/:outing_id/users", to: "outings#join"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
